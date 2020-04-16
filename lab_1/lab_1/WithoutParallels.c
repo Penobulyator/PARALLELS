c381f02738a96714297ffb92b461419b6ee33e08
penobulyator <dima.onishuk70@gmail.com>
2020-04-16 22:43:45 +0700

    Fixed WithOutparallels.c. Added time measurement with gettimeofday()

--- lab_1/lab_1/WithoutParallels.c
+++ lab_1/lab_1/WithoutParallels.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <time.h>
-#define N  1000
+#include <time.h> 
+
+#define N 1024
 double** createMatrix()
 {
 	double** out = (double**)malloc(N * sizeof(double*));
@@ -71,29 +72,42 @@ void printVector(double* vector)
 	}
 	printf("\r\n");
 }
+void countDiff(double* diff, double *const *const A, double *const b, double *const x)
+{
+	//count Ax
+	for (int i = 0; i < N; i++)
+	{
+		diff[i] = 0;
+		for (int j = 0; j < N; j++)
+			diff[i] += A[i][j] * x[j];
+	}
+	//count Ax - b
+	for (int i = 0; i < N; i++)
+		diff[i] -= b[i];
+}
+void refreshRoot(double* root, double *const diff, const double t)
+{
+	//root = root - t * diff
+	for (int i = 0; i < N; i++)
+		root[i] = root[i] - t * diff[i];
+}
 double* root(double** const A, double* const b)
 {
 	double* cur_root = (double*)calloc(N, sizeof(double));
-	double t = 0.0001;
-	const double epsilon = 0.00001;
+	double* diff = (double*)calloc(N, sizeof(double));
+	const double t = 0.0001;
+	const double epsilon = 0.001;
 	while (1)
 	{
-		double* mult = matrixMULTvectror(A, cur_root);
-		double* diff = vectorMINUSvector(mult, b);
+		countDiff(diff, A, b, cur_root);
 		if (metric(diff) / metric(b) < epsilon)
 		{
-			free(mult);
 			free(diff);
 			return cur_root;
 		}
 		else
 		{
-			double* tdiff = multScal(diff, t);
-			double* new_root = vectorMINUSvector(cur_root, tdiff);
-			cur_root = new_root;
-			free(tdiff);
-			free(mult);
-			free(diff);
+			refreshRoot(cur_root, diff, t);
 		}
 	}
 	
@@ -102,12 +116,16 @@ int main()
 {
 	double** A = createMatrix();
 	for (int i = 0; i < N; i++)
-		A[i][i] = i + 1;
+		A[i][i] = 1;
 	double* b = createVector();
 	for (int i = 0; i < N; i++)
-		b[i] = i + 1;
-	double* x = root(A, b);
-	printVector(x);
+		b[i] = 1; 
+	clock_t t = clock();
+	double* x = root(A, b); 
+	t = clock() - t;
+	for (int i = 0; i < N; i++)
+		printf("%2f\n", x[i]);
+	printf("time diff: %e \n", ((double)t) / CLOCKS_PER_SEC);
 	deleteMatrix(A);
 	free(b);
 	return 0;
